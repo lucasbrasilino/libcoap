66e470090106816f24738649c29d09f01f53df09
Lucas Brasilino <lbrasili@indiana.edu>
2019-03-27 13:12:41 -0400

    urand URI Path

--- examples/coap-server.c
+++ examples/coap-server.c
@@ -25,6 +25,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <signal.h>
+#include <fcntl.h>
 
 #include "coap_config.h"
 #include "resource.h"
@@ -164,11 +165,8 @@ hnd_get_cycles(coap_context_t  *ctx,
              coap_pdu_t *request,
              str *token,
              coap_pdu_t *response) {
-  coap_opt_iterator_t opt_iter;
-  coap_opt_t *option;
   unsigned char buf[8];
   size_t len = 8;
-  time_t now;
   coap_tick_t t;
   static uint64_t cycles_number = 0;
   
@@ -180,7 +178,6 @@ hnd_get_cycles(coap_context_t  *ctx,
 
     /* calculate current time */
     coap_ticks(&t);
-    now = my_clock_base + (t / COAP_TICKS_PER_SECOND);
     cycles_number++;
     strncpy ((char *)buf, (char *)&cycles_number,len);
     coap_add_data(response, len, buf);
@@ -224,6 +221,40 @@ hnd_get_temp(coap_context_t  *ctx,
     strncpy ((char *)buf, (char *)&temperature,len);
     coap_add_data(response, len, buf);
    
+}
+
+static void
+hnd_get_urand(coap_context_t  *ctx,
+             struct coap_resource_t *resource,
+             const coap_endpoint_t *local_interface UNUSED_PARAM,
+             coap_address_t *peer,
+             coap_pdu_t *request,
+             str *token,
+             coap_pdu_t *response) {
+  coap_opt_iterator_t opt_iter;
+  coap_opt_t *option;
+  unsigned char buf[8];
+  size_t len = 8;
+  time_t now;
+  coap_tick_t t;
+  static char filename[]="/dev/urandom";
+  static int fd;
+  static long unsigned int value;
+
+  response->hdr->code = COAP_RESPONSE_CODE(205);
+
+  coap_add_option(response,
+                    COAP_OPTION_CONTENT_FORMAT,
+                    coap_encode_var_bytes(buf, COAP_MEDIATYPE_APPLICATION_OCTET_STREAM), buf);
+
+  if ((fd = open (filename,O_RDONLY)) < 0 ) {
+      fprintf (stderr,"Can't open %s\n",filename);
+      exit(1);
+  }
+  read (fd, &value, sizeof (value));
+  value += (value<<1);
+  coap_add_data (response,len,(char *)&value);
+  close(fd);
 }
 
 static void
@@ -391,8 +422,11 @@ init_resources(coap_context_t *ctx) {
 
   r = coap_resource_init((unsigned char *)"temp", 4, COAP_RESOURCE_FLAGS_NOTIFY_CON);
   coap_register_handler(r, COAP_REQUEST_GET, hnd_get_temp);
-  coap_add_resource(ctx, r); 
+  coap_add_resource(ctx, r);
 
+  r = coap_resource_init((unsigned char *)"urand", 5, COAP_RESOURCE_FLAGS_NOTIFY_CON);
+  coap_register_handler(r, COAP_REQUEST_GET, hnd_get_urand);
+  coap_add_resource(ctx, r); 
   
 #ifndef WITHOUT_ASYNC
   r = coap_resource_init((unsigned char *)"async", 5, 0);
